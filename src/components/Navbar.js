f6fbea41c56b0081684eca72e6fad4d4dc1231cd
RealMinTech <adesinamariam7@gmail.com>
2025-07-09 23:53:21 +0100

    debug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -76,7 +76,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
+                to="#"
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineFundProjectionScreen
